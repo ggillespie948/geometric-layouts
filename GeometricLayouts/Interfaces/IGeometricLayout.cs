84cb663a375093dab30c2c9a238a7d0b0fe880c0
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-22 09:59:10 +0100

    add geometric layout interface

---
+++ GeometricLayouts/Interfaces/IGeometricLayout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometricLayouts.Interfaces
+{
+    public interface IGeometricLayout
+    {
+        IEnumerable<IShape> GenerateLayout();
+        IShape GenerateShapeFromId(string Id);
+    }
+}
