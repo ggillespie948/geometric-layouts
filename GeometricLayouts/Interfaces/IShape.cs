a9c5025f260cf13e84643ea76e44d458b2c74c78
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-22 09:55:46 +0100

    add IShape interface

---
+++ GeometricLayouts/Interfaces/IShape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometricLayouts.Interfaces
+{
+    public interface IShape
+    {
+        string Id { get;  }
+
+        string VerticesToString { get; }
+    }
+}
