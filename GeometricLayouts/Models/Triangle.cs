187944ea817861c2088606b43cfe019ca08db1c9
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-22 10:27:18 +0100

    add initial triangle, triangle layout implementations

---
+++ GeometricLayouts/Models/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using GeometricLayouts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometricLayouts.Models
+{
+    public class Triangle : IShape
+    {
+        public string Id { get; private set; }
+
+        public Point V1 { get; private set; }
+        public Point V2 { get; private set; }
+        public Point V3 { get; private set; }
+
+        public Triangle(string Id, int V1x, int V1y, int V2x, int V2y, int V3x, int V3y)
+        {
+            this.Id = Id;
+            this.V1 = new Point(V1x, V1y);
+            this.V1 = new Point(V2x, V3y);
+            this.V1 = new Point(V3x, V3y);
+        }
+
+        public string VerticesToString => throw new NotImplementedException();
+    }
+}
---
+++ GeometricLayouts/Models/TriangleLayout.cs
@@ -0,0 +1,21 @@
+﻿using GeometricLayouts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometricLayouts.Models
+{
+    public class TriangleLayout : IGeometricLayout
+    {
+        public IEnumerable<IShape> GenerateLayout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IShape GenerateShapeFromId(string Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
