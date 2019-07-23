e12534c41ece7d0bc3c340e7e1be2be6dd5e56b4
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-23 11:38:32 +0100

    add axios, retrieve all triangles from API

--- geometric-layouts-ui/src/App.js
+++ geometric-layouts-ui/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    triangles: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://localhost:44382/api/trianglelayout", { crossdomain: true }) //note: local host port may differ
+      .then(data => {
+        this.setState({triangles: data.data}); 
+        console.log(this.state.triangles);
+      }).catch(function(error) {
+        console.log("Error fetching data form api: " + error.data)
+      })
+  }
+
   render() {
+    
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Geometric Layouts</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
